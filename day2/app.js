4684c4e06256758d2c4a8b10701cc39c90fa88ea
whoamitty <74999206+whoamitty@users.noreply.github.com>
2023-12-18 22:20:59 -0300

    What I deleted
    variables useless, and init as:
    
    (let listNumberColor) in lineToTable
    concatGamesTrue=""
    sumIndexGames=0
    sumAllTrueElements=0
    sumAllElements=0
    sumAllElementsPlusIDs=0
    
    The functions:
    
    statusGame1()
    searchNumberColor(line,regexColor)
    gameSumElements(line)
    
    I renamed function
    searchNumberColor2 to => searchNumberColor
    because I had an alone type searchNumberColor function
    after deleted searchNumberColor

--- day2/app.js
+++ day2/app.js
@@ -32,132 +32,53 @@ regexBlue=/\d+(?= blue)/g
 var regexColors=[['red',regexRed],['green',regexGreen],['blue',regexBlue]]
 
 lineNumber = 0;
-concatGamesTrue="";
-sumIndexGames=0;
 sumGamesTrue=0;
 listGamesTrue="";
-sumAllTrueElements=0
-sumAllElements=0
-sumAllElementsPlusIDs=0
+
 
 // Écouter l'événement 'line' qui est émis à chaque fois qu'une nouvelle ligne est lue
 rl.on('line', (line) => {
     lineNumber++;
-    sumIndexGames=Number(sumIndexGames)+Number(lineNumber)
-    
-    
-    
-    /* 
-    if (statusGame1(line)) {console.log(statusGame1(line),"Yes")}
-    else {console.log(statusGame1(line),"No")};
-    */
-    
-    console.log("Nombre de bleu dans le premier tiragee :",searchNumberColor2(lineToTable(line)[0],regexBlue))
-    console.log("sum:",gameSumElements(line))
+    console.log("Nombre de bleu dans le premier tirage :",searchNumberColor(lineToTable(line)[0],regexBlue))
+    console.log(line);
     console.log(lineToTable(line))
-    console.log(statusGame1(line),"\n\n");
     
-    // console.log(`Ligne ${lineNumber}: ${line}`);
-    
-    sumAllElements=sumAllElements+gameSumElements(line) //+Number(lineNumber);
-    sumAllElementsPlusIDs=sumAllElementsPlusIDs+gameSumElements(line) +Number(lineNumber);
-    if (statusGame1(line)){
-
-        sumGamesTrue=Number(sumGamesTrue)+Number(lineNumber)
-        concatGamesTrue=concatGamesTrue.toString()+lineNumber.toString();
-        listGamesTrue=listGamesTrue.toString()+ " " + lineNumber.toString();
-        sumAllTrueElements=sumAllTrueElements+gameSumElements(line) //+Number(lineNumber);
-    }
-
-
-
-    
+    console.log("\n")
 });
 
-
-function gameSumElements(line){
-    let numberColor=0;
-
-    for (const regex of [regexRed,regexGreen,regexBlue]){
-    numberColor=numberColor+searchNumberColor(line,regex);
-    }
+function searchNumberColor(subSetofColor,regexColor){
     
-    return numberColor;
-}
-
-
-/* 
-Red.trim()
-Green
-Blue
-*/
-
- 
-
-function statusGame1(line){
-    
-    for (const color of regexColors) {
-        
-        let number=searchNumberColor(line, color[1]);
-        // console.log("searchNumberColor in function statusGame1:",   color[0], number);
-        if (statusColor(color[0], number)){
-            "pass";
-        }
-        else{return false;}
-    }
-
-    
-
-    return true;
-}
-/* ATTENTION, ce n'est pas lui ("Color2") !!!!! */
-function searchNumberColor(line,regexColor){ ///!!!!!!!!!!!
-
-
-    let listNumberColor=line.match(regexColor);
-    let numberColor=0;
-
-
-    listNumberColor.forEach(
-        number => {
-            numberColor=Number(numberColor)+Number(number);
-    });
-    return numberColor
-}
-
-
-
-
-function searchNumberColor2(subSetofColor,regexColor){
-
     let numberColor=subSetofColor.match(regexColor)
-    if (!/null/.test(numberColor)){
-        return 0;
-        
-    }
-    console.log("Hello Hello:",Number(numberColor))
     return Number(numberColor)
 
 }
 
+
+
 // ##############################
 function lineToTable(line) {
-    let listNumberColor
-    // const config=line.split(': ')[1].map((x) => x.trim())
     const config=line.split(': ')[1].trim()
-    // let set=config.split(";").map((x) => x.trim())
     let setofTable=config.split(";")
-    
     return setofTable
-
 
     }
 
+    
+    
+// Écouter l'événement 'close' et effectuer une action une fois que le fichier a été lu entièrement
+rl.on('close', () => {
+    console.log('Fin de la lecture du fichier.');
+    // console.log(argv[2]);
+});
 
+// Gérer les erreurs éventuelles
+rl.on('error', (err) => {
+    console.error('Une erreur est survenue lors de la lecture du fichier:', err);
+    
+});
 
 
 function statusColor(color, number) {
-
     if (color === "red") {
         if (number > 12) { return false; }
     }
@@ -173,26 +94,8 @@ function statusColor(color, number) {
     return true;
 }
 
-
-// Écouter l'événement 'close' et effectuer une action une fois que le fichier a été lu entièrement
-rl.on('close', () => {
-
-    console.log("concatGamesTrue:",concatGamesTrue);
-    console.log("listGamesTrue:",listGamesTrue); 
-    console.log("sumGamesTrue:",sumGamesTrue);
-    console.log("sumIndexGames:",sumIndexGames); 
-    console.log("sumAllTrueElement:",sumAllTrueElements); 
-    console.log("sumAllElements:",sumAllElements); 
-    console.log("sumAllElementsPlusIDs:",sumAllElementsPlusIDs); 
-    console.log("concatGamesTrue+sumIndexGames:",concatGamesTrue+sumIndexGames.toString());
-    console.log("sumIndexGames+concatGamesTrue:",sumIndexGames.toString()+concatGamesTrue);
-
-    // console.log(argv[2]);
-
-    console.log('Fin de la lecture du fichier.');
-});
-
-// Gérer les erreurs éventuelles
-rl.on('error', (err) => {
-    console.error('Une erreur est survenue lors de la lecture du fichier:', err);
-});
+/* 
+Red.trim()
+Green
+Blue
+*/
---
+++ day2/avancement_par_lives.md
@@ -0,0 +1,100 @@
+Les récap du jour
+
+# 6 14-12-2023 Jeudi  
+
+Problemes réglés:  
+ajout de !/null/.test dans "searchNumberColor2"  
+pour tester si une valeur est null avant de retouner le résultat
+
+source  
+https://www.benpickles.com/articles/60-what-happens-when-null-is-passed-to-a-javascript-regular-expression
+
+
+Problematique final :  
+est ce que je met le code de "statusColor"  
+dans "searchNumberColor2" pour faire tout en une fois  
+
+Je pense que je le ferrais
+
+
+
+
+
+# 7 15-12-2023 vendredi
+
+C'est décevant, mais aujourd'hui,  
+pas d'avancement  
+peut-être la fatigue ?  
+
+Il y a eu aussi un gros retard de 1h à cause de mauvaises manipes :  
+2 arrêts du live sans le faire expres  
+
+Et pour contre-temps  
+
+
+Ça ne marche pas comme ça devrais  
+Pourquoi ça ne s'affiche qu'à la fin ?  
+console.log("Nombre de bleu dans le premier tiragee :",searchNumberColor2(lineToTable(line)[0],regexBlue))  
+
+Je ne comprend même plus, certains points de mon code  
+
+À partir de maintenant, j'utiliserais l'IA 1 jour/2  
+
+
+
+# 8 17-12-2023 Dimanche
+
+
+<details >
+  <summary>Tout ce qui as été fait aujourd'hui</summary>
+Pour Aujourd'hui :  
+
+1. C'est full IA  
+
+X 2. Il serais bien de comprendre  
+pourquoi "searchNumberColor2"  
+semble s'exécuter seulement à la fin.  
+
+X 3. je vais essayer de néttoyer le code  
+pour y voir plus clair.  
+
+
+
+### Ce qu'il faut supprimer
+#### Les fonctions:  
+X 1. La fonction statusGame1()
+
+X 2. searchNumberColor(line,regexColor)
+
+X 3. gameSumElements(line)
+
+#### Les "Appelles" de fonction :
+X les "console.log" en fin d'exécution, il y en as trop, ça embrouille.  
+X ...
+
+
+#### variables useless, and init as:  
+
+X (let listNumberColor) in lineToTable  
+X concatGamesTrue=""   
+X sumIndexGames=0   
+X sumAllTrueElements=0    
+X sumAllElements=0  
+X sumAllElementsPlusIDs=0 
+
+## I renamed
+searchNumberColor2 en => searchNumberColor
+<details >
+
+
+# 9 18-12-2023 Lundi
+
+
+
+Essayons d'intégrer statusColor(color, number)
+à notre code
+
+et de généraliser au test de toutes les couleur
+sur chaque ensemble d'elements
+
+
